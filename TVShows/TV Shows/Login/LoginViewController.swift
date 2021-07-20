2c241a7b80de1c8076b84986579ee49b738ee1ed
Matija Barošević <>
2021-07-20 20:10:17 +0200

    TV Shows application redesign

--- TVShows/TV Shows/Login/LoginViewController.swift
+++ TVShows/TV Shows/Login/LoginViewController.swift
@@ -8,72 +8,109 @@
 import Foundation
 import UIKit
 import SVProgressHUD
+import Alamofire
+
+struct LoginUser: Codable {
+    let email: String
+    let imageUrl: String?
+    let id: String
+    
+    enum CodingKeys: String, CodingKey {
+        case email
+        case imageUrl = "image_url"
+        case id
+    }
+}
+
+struct LoginResponse: Codable {
+    let user: LoginUser
+}
 
 final class LoginViewController: UIViewController {
 
-    @IBOutlet private weak var showNumberOfTapsLabel: UILabel!
-    @IBOutlet private weak var touchCounterButton: UIButton!
-    @IBOutlet private weak var changeStateButton: UIButton!
-    @IBOutlet private weak var loadingIndicator: UIActivityIndicatorView!
-    
-    private var numberOfClicks: Int = 0
+    @IBOutlet private weak var usernameInputTextField: UITextField!
+    @IBOutlet private weak var passwordInputTextField: UITextField!
+    @IBOutlet private weak var checkboxButton: UIButton!
+    @IBOutlet private weak var loginButton: UIButton!
+    private var rememberMe: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addRoundedEdgesToTheButton()
-        autoStartActivityIndicator()
-        //displaySVProgressHUD()
+        addRoundedEdgesToTheLoginButton()
+        updateDesignOfUITextField()
+        //signIn()
     }
     
-    private func addRoundedEdgesToTheButton() {
-        touchCounterButton.layer.cornerRadius = 20
-        touchCounterButton.layer.borderWidth = 0.2
-        touchCounterButton.layer.borderColor = UIColor.gray.cgColor
-    }
-    
-    private func autoStartActivityIndicator(){
-        loadingIndicator.startAnimating()
-        loadingIndicator.isHidden = false
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3){ [self] in
-            loadingIndicator.stopAnimating()
-            loadingIndicator.isHidden = true
-        }
-    }
-    
-    private func startStopActivityIndicator() {
-        if loadingIndicator.isAnimating {
-            loadingIndicator.stopAnimating()
-            loadingIndicator.isHidden = true
-            changeStateButton.setTitle("Start", for: .normal)
+    @IBAction private func touchRememberMeCheckbox() {
+        if !rememberMe {
+            checkboxButton.setImage(UIImage(named: "ic-checkbox-selected"), for: .normal)
+            self.rememberMe = true
         } else {
-            loadingIndicator.startAnimating()
-            loadingIndicator.isHidden = false
-            changeStateButton.setTitle("Stop", for: .normal)
+            checkboxButton.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
+            self.rememberMe = false
         }
     }
     
-    // Display progress HUD for 5 seconds
-    private func displaySVProgressHUD() {
-        // Simple SVProgressHUD example, a dispatch queue dismisses the progress hud after 5 seconds of displayed progress hud
-        // A great use case is to display progress hud after Login btn click and while API call is being performed
+    private func addRoundedEdgesToTheLoginButton() {
+        loginButton.layer.cornerRadius = 21.5
+    }
+    
+    private func updateDesignOfUITextField() {
+        usernameInputTextField.setLeftPaddingPoints(10)
+        passwordInputTextField.setLeftPaddingPoints(10)
+
+        setBottomLine(to: usernameInputTextField)
+        setBottomLine(to: passwordInputTextField)
         
+        setPlaceholderText(to: usernameInputTextField, value: "ios.team@infinum.com")
+        setPlaceholderText(to: passwordInputTextField, value: "••••••••••")
+    }
+    
+    private func setBottomLine(to textField: UITextField) {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(
+            x: 0.0,
+            y: textField.frame.height - 1,
+            width: textField.frame.width,
+            height: 1.0)
+        bottomLine.backgroundColor = UIColor.white.cgColor
+        textField.borderStyle = UITextField.BorderStyle.none
+        textField.layer.addSublayer(bottomLine)
+    }
+    
+    private func setPlaceholderText(to textField: UITextField, value: String) {
+        textField.attributedPlaceholder = NSAttributedString(string: value, attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+    }
+    
+    private func signIn() {
+        let params: [String: String] = [
+                    "email": "mbarosevic@gmail.com",
+                    "password": "tester1234",
+                ]
         SVProgressHUD.show()
-        // Closure
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5){
-            SVProgressHUD.dismiss()
-        }
+        AF
+            .request(
+                "https://tv-shows.infinum.academy/users/sign_in",
+                method: .post,
+                parameters: params,
+                encoder: JSONParameterEncoder.default)
+            .validate()
+            .responseDecodable(of: LoginResponse.self) { response in
+                SVProgressHUD.dismiss()
+                switch response.result {
+                case .success(let response):
+                    print("OK \(response)")
+                case .failure(let error):
+                    print("Error2 \(error)")
+                }
+            }
     }
-    
-    @IBAction private func buttonActionHandler() {
-        // Console output
-        print("Button tapped")
-        
-        // numberOfTaps variable increment on each tap of button
-        numberOfClicks += 1
-        showNumberOfTapsLabel.text = String(numberOfClicks)
-    }
-    
-    @IBAction private func changeActivityIndicatorVisibilityButton() {
-        startStopActivityIndicator()
+}
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
     }
 }
